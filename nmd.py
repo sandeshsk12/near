30a9deebcef1868649eaf64e6e4d5abeb25cc7c6
sksandeshkandagal@gmail.com <sksandeshkandagal@gmail.com>
2023-02-04 10:07:56 +0530

    changed font color

--- nmd.py
+++ nmd.py
@@ -669,7 +669,12 @@ with dashboard_in_depth:
         # delta = {'position': "bottom", 'reference': round(float(dao_twitter['Followers'][1]),3)},
         domain = {'x': [0, 1], 'y': [0, 1]}))
     fig.update_layout({'plot_bgcolor': 'rgba(100, 0, 0, 0)','paper_bgcolor': 'rgba(215,215,215,255)',})
-    fig.update_layout(height=220, width=600)
+    fig.update_layout(height=220, width=600)    
+    fig.update_layout(font=dict(
+            # family="Courier New, monospace",
+            # size=18,
+            color="black"
+        ))
     c2.plotly_chart(fig,use_container_width=True)
     c1.plotly_chart(top_target_wallets_fig, use_container_width=True)
 
